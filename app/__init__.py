a54561cde77b374460ab0fccf53db2a0a0b3f288
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-17 10:06:51 -0700

    Adds customer and video model migrations

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -20,10 +21,15 @@ def create_app(test_config=None):
             "SQLALCHEMY_TEST_DATABASE_URI")
 
     # import models here
+    from app.models.customer import Customer
+    from app.models.video import Video
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # register Blueprints here
+    from .routes import customers_bp, videos_bp
+    app.register_blueprint(customers_bp)
+    app.register_blueprint(videos_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,6 @@
 from flask import Blueprint, request, make_response, jsonify
 from app import db
 import os
+
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
