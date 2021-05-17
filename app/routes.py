a04ece3c9e9a33c14a61cdcf1fda9ce81c592843
Brittney Payne <ohoptional@gmail.com>
2021-05-17 12:19:41 -0700

    instanciated bp's for video and cust, migrate db

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from flask import Blueprint
+from app.models.Videos import Video 
+from app.models.Customers import Customer
+from flask import Blueprint, make_response, jsonify, request
+from app import db 
+from datetime import date
+import requests 
+import os 
+
+customer_bp = Blueprint("customers", __name__, url_prefix="/customers")
+video_bp = Blueprint("videos", __name__, url_prefix="/videos")
+
+
+
+
 
