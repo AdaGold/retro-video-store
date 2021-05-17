c724756841c5d721d439010f7da58b53b2d6c544
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-16 21:10:22 -0700

    Adds Customer and Video models

---
+++ app/models/video.py
@@ -0,0 +1,8 @@
+from flask import current_app
+from app import db
+
+class Video(db.Model):
+    video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime)
+    total_inventory = db.Column(db.Integer)
