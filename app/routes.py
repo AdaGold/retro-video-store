fe6935fe353c013cdc732b83e26d7bfa063dbd57
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-17 14:51:53 -0700

    - GET method for /customers

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,18 @@ from datetime import datetime
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
+
+# -------------- CRUD for /customers ------------------------
+
+
+@customers_bp("", methods=["GET"])
+def get_all_customers():
+    """
+    Lists all existing customers and details about each customer.
+    """
+    customers = Customer.query.all()
+    response = [customer.to_json() for customer in customers]
+    return response
+
+
+# -------------- CRUD for /videos ---------------------------
