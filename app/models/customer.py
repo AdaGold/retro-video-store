e3021fc34494e88f3f0156b7c76fcbe3ed199dd6
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-17 14:33:11 -0700

    - create Customer model - create Video model - create .env - set enviromental variables - configures the app to connect to db - initialized models to become availible in app - add .gitignore - create a migration repository

---
+++ app/models/customer.py
@@ -0,0 +1,29 @@
+from flask import current_app
+from app import db
+
+
+class Customer(db.Model):
+    customer_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    registered_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_json(self):
+        """Converts a Customer instance into JSON"""
+        response_body = {
+            "id": self.customer_id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone": self.phone,
+            "registered_at": self.registered_at
+        }
+        return response_body
+
+    def from_json(self, json):
+        """Converts JSON into a new instance of Customer"""
+        self.name = json["name"]
+        self.postal_code = json["postal_code"]
+        self.phone = json["phone"]
+        self.registered_at = json["registered_at"]
+        return self
---
+++ app/models/video.py
@@ -0,0 +1,29 @@
+from flask import current_app
+from app import db
+
+
+class Video(db.Model):
+    video_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    total_inventory = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer)
+    release_date = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_json(self):
+        """Converts a Video instance into JSON"""
+        response_body = {
+            "id": self.video_id,
+            "title": self.title,
+            "total_inventory": self.total_inventory,
+            "available_inventory": self.available_inventory,
+            "release_date": self.release_date
+        }
+        return response_body
+
+    def from_json(self, json):
+        """Converts JSON into a new instance of Video"""
+        self.title = json["title"]
+        self.total_inventory = json["total_inventory"]
+        self.available_inventory = json["available_inventory"]
+        self.release_date = json["release_date"]
+        return self
