74571c90ac3ef754dcadf2598e59327c87da65ff
Sidney DuPont <sidneyadupont@gmail.com>
2021-05-17 11:38:23 -0700

    Adds customer HTTP request routes

--- app/models/customer.py
+++ app/models/customer.py
@@ -5,5 +5,26 @@ class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     postal_code = db.Column(db.Integer)
-    phone_number = db.Column(db.Integer)
-    register_at = db.Column(db.DateTime)
+    phone = db.Column(db.Integer)
+    registered_at = db.Column(db.DateTime, nullable=True)
+
+    def to_json(self):
+        """
+        Outputs formatted JSON dictionary of customer attributes
+        """
+        return {
+            "id": self.customer_id,
+            "name": self.name,
+            "postal_code": self.postal_code,
+            "phone": self.phone,
+            "registered_at": self.registered_at
+            }
+
+    def from_json(self, input_data):
+        """
+        Converts JSON input data into new instance of Customer
+        """
+        return self(name=input_data["name"],
+        postal_code=input_data["postal_code"],
+        phone=input_data["phone"],
+        registered_at=input_data["registered_at"])
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,93 @@
 from flask import Blueprint, request, make_response, jsonify
+from app.models.customer import Customer
+from app.models.video import Video
 from app import db
 import os
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
+
+@customers_bp.route("", methods=["GET"])
+def get_customers():
+    '''
+    Gets list of all customers
+    '''
+    customers = Customer.query.all()
+
+    customers_response = []
+    for customer in customers:
+        customers_response.append(customer.to_json())
+    return jsonify(customers_response)
+
+@customers_bp.route("", methods=["POST"])
+def post_customer():
+    '''
+    Posts new customer
+    '''
+    request_body = request.get_json()
+
+    if "name" not in request_body or "postal_code" not in request_body\
+        or "phone" not in request_body:
+        return ({
+            "errors": ["Not Found"]
+        }, 400)
+    else:
+        new_customer = Customer.from_json(Customer, request_body)
+
+        db.session.add(new_customer)
+        db.session.commit()
+
+        return {
+            "id": new_customer.customer_id
+        }, 201
+
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_customer(customer_id):
+    '''
+    Gets customer by customer_id
+    '''
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response("No customer found", 404)
+
+    return {
+        "customer": customer.to_json()
+    }
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    '''
+    Updates specific customer information
+    '''
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response("No customer found", 404)
+
+    form_data = request.get_json()
+
+    customer.name = form_data["name"]
+    customer.postal_code = form_data["postal_code"]
+    customer.phone = form_data["phone"]
+    customer.registered_at = form_data["registered_at"]
+
+    db.session.commit()
+
+    return {
+        "customer": customer.to_json()
+    }
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    '''
+    Deletes customer by customer_id
+    '''
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response("No customer found", 404)
+
+    db.session.delete(customer)
+    db.session.commit()
+
+    return {
+        "id": customer.customer_id
+    }
