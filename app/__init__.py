36c89cacd7f048ab437f60b69e37aa9d5f75a729
Marisa <marisa.n.morales@gmail.com>
2021-05-17 19:10:16 -0700

    initail project set up. Adds info to app/init to make migration possible

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    
+    # Import models here for Alembic setup
+    # from app.models.task import 
+    # from app.models.goal import 
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+
 
     return app
