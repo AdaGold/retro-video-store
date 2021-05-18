1c481905b929f1f9a5abbdf1b60a6172ae5680fb
Sara <sarajessvega17@gmail.com>
2021-05-18 13:50:46 -0500

    POST and GET methods for videos added

--- app/models/video.py
+++ app/models/video.py
@@ -2,9 +2,18 @@ from flask import current_app
 from app import db
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from .customer import Customer
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime, nullable=True, default=None)
-    total_inventory = db.Column(db.String)
+    total_inventory = db.Column(db.Integer)
+
+    def get_response(self):
+        return {
+            "id":self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "total_inventory": int(self.total_inventory)}
+            # "available_inventory":0 
--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,7 @@ customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 
 '''
-To Do:
-PUT /customers/<id>
-DELETE /customers/<id>
+CRUD routes for Customers
 '''
 @customers_bp.route("", methods=["GET"])
 def get_all_customers():
@@ -74,11 +72,54 @@ def delete_customer(id):
         "id":customer.id
     }, 200
 
-
 def valid_customer_data(request_body):
     if "name" not in request_body or "postal_code" not in request_body or "phone" not in request_body:
         return False
-        # How can you validate data types for not required params?
+    # How can you validate data types for not required params?
     # elif not isinstance(request_body["name"],str) or not isinstance(request_body["phone"],str) or not isinstance(request_body["postal_code"],str):
     #     return False
+    return True
+
+'''
+CRUD routes for Videos
+
+POST /videos
+PUT /videos/<id>
+DELETE /videos/<id>
+'''
+
+@videos_bp.route("", methods=["GET"])
+def get_all_videos():
+    videos = Video.query.all()
+    videos_list = []
+    for video in videos:
+        videos_list.append(video.get_response())
+
+    return jsonify(videos_list), 200
+
+@videos_bp.route("/<id>", methods=["GET"])
+def get_video_by_id(id):
+    video = Video.query.get(id)
+
+    if video == None: 
+        return {"error":f"Video ID {id} not found."}, 404
+    return jsonify(video.get_response()),200
+
+@videos_bp.route("", methods=["POST"])
+def create_new_video():
+    request_body = request.get_json()
+    if not valid_video_data(request_body):
+            return {"details":"Invalid data"}, 400
+    new_video = Video(
+                        title=request_body["title"],
+                        release_date=request_body["release_date"],
+                        total_inventory=request_body["total_inventory"])
+    db.session.add(new_video)
+    db.session.commit()
+    return {"id":new_video.id},201
+
+
+def valid_video_data(request_body):
+    if "title" not in request_body or "release_date" not in request_body or "total_inventory" not in request_body:
+        return False
     return True
