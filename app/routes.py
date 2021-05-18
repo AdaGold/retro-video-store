23bfdbc6e04a87be14bc9f4f84640d1beb7a506c
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-17 19:41:45 -0700

    - PUT method for /customers/<customer_id> - DELETE method for /customers/<customer_id>

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ def get_all_customers():
     """Lists all existing customers and details about each customer."""
     customers = Customer.query.all()
     response = [customer.to_json() for customer in customers]
-    return jsonify(response)
+    return jsonify(response), 200
 
 
 @customers_bp.route("", methods=["POST"])
@@ -28,7 +28,10 @@ def add_customer():
             "name" in request_body and "postal_code" in request_body and "phone" in request_body):
         return make_response({
             "errors": [
-                "Invalid input data"
+                "Bad Request",
+                "'name' is required",
+                "'postal_code' is required",
+                "'phone' is required"
             ]
         }, 400)
 
@@ -38,7 +41,7 @@ def add_customer():
     db.session.commit()
 
     # retrieve_customer = Customer.query.get(new_customer.customer_id)
-    return make_response({"id": new_customer.customer_id}, 201)
+    return make_response(new_customer.to_json(), 201)
 
 
 @customers_bp.route("/<customer_id>", methods=["GET"])
@@ -54,3 +57,59 @@ def get_customer_by_id(customer_id):
             ]
         }, 400)
     return make_response(customer.to_json(), 200)
+
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer_by_id(customer_id):
+    """Updates and returns details about specific customer."""
+    customer = Customer.query.get(customer_id)
+
+    if customer is None:
+        return make_response({
+            "errors": [
+                "Not Found",
+                "Customer to update does not exist"
+            ]
+        }, 400)
+
+    request_body = request.get_json()
+
+    if not (
+            "name" in request_body and "postal_code" in request_body and "phone" in request_body):
+        return make_response({
+            "errors": [
+                "Bad Request",
+                "'name' is required",
+                "'postal_code' is required",
+                "'phone' is required"
+            ]
+        }, 400)
+
+    customer = customer.from_json(request_body)
+    db.session.commit()
+    retrieve_customer = Customer.query.get(customer_id)
+
+    return make_response(retrieve_customer.to_json(), 200)
+
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer_by_id(customer_id):
+    """Deletes a specific customer."""
+    customer = Customer.query.get(customer_id)
+
+    if customer is None:
+        return make_response({
+            "errors": [
+                "Not Found",
+                "Customer to delete does not exist"
+            ]
+        }, 404)
+
+    db.session.delete(customer)
+    db.session.commit()
+    # return ({"id": customer_id, "details": 'Task has been successfully
+    # deleted'}, 200)
+    return ({"id": customer_id}, 200)
+
+# -------------- CRUD for /videos ------------------------
+
