0cc44221d4555431b16af64e0c8edf736d335b2b
Summer <summmick@gmail.com>
2021-05-17 21:29:56 -0700

    Create join table for customer and video

--- app/customer_routes.py
+++ app/customer_routes.py
@@ -4,3 +4,11 @@ from app.models.customer import Customer
 from datetime import datetime
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+@customers_bp.route("", methods=["POST"], strict_slashes=False)
+def create_customer():
+    body = request.get_json() 
+
+    new_customer = Customer(name = body["name"],
+                            postal_code = body["postal_code"],
+                            phone_number = body["phone_number"])
--- app/models/customer.py
+++ app/models/customer.py
@@ -1,6 +1,7 @@
 from flask import current_app
 from app import db
 from sqlalchemy import DateTime
+from app.models import customer_video_join  
 
 
 class Customer(db.Model):
@@ -9,4 +10,5 @@ class Customer(db.Model):
     postal_code = db.Column(db.String)
     phone_number = db.Column(db.String)
     register_at = db.Column(db.DateTime, nullable=True)
+    videos = db.relationship("Video", secondary=customer_video_join)
     
