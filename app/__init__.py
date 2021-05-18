a6f4c9e4e600813e09d8ecab2e363130d7a34f81
Hannah Lumapas <lumapas.h@gmail.com>
2021-05-18 14:56:35 -0400

    creates Customer model, connects to the correct database, & registers Blueprint for customers_bp

--- app/__init__.py
+++ app/__init__.py
@@ -17,18 +17,22 @@ def create_app(test_config=None):
     if test_config is None:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
-    else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+    # else:
+    #     app.config["TESTING"] = True
+    #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    #         "SQLALCHEMY_TEST_DATABASE_URI")
 
     # Import models here for Alembic setup
-    # from app.models.task import Task
+    from app.models.customer import Customer
     # from app.models.goal import Goal
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
+    from .routes import customers_bp
+    app.register_blueprint(customers_bp)
+    # from .routes import goals_bp
+    # app.register_blueprint(goals_bp)
 
     return app
