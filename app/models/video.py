9fe826a6881370390751453a1e56b0cd08dd18d0
Jittania <jittania@gmail.com>
2021-05-17 22:51:17 -0700

    Set up Models for Wave 1

---
+++ app/models/video.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Video(db.Model):
+    video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime)
+    tot_inventory = db.Column(db.Integer)
---
+++ notes_and_resources.md
@@ -0,0 +1,4 @@
+SQLAlchemy documentation for many-to-many relationships ~ make sure to check SQLAlchemy version (?)
+
+Creating Many-To-Many Relationships in Flask-SQLAlchemy:
+https://www.youtube.com/watch?v=OvhoYbjtiKc
