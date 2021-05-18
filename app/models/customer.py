cf06c94782919f3fe800b9802d09f1ec4f59db33
kmalakhova <kgrekhova@gmail.com>
2021-05-17 18:23:33 -0700

    Add Customer and Video models

---
+++ app/models/customer.py
@@ -0,0 +1,20 @@
+from app import db
+# from sqlalchemy.orm import relationship
+
+class Customer(db.Model):
+    customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    registered_at = db.Column(db.DateTime(), nullable=True)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    videos_checked_out_count = db.Column(db.Integer, default=0)
+
+    def to_dict(self): 
+        return {
+            "id": self.customer_id,
+            "name": self.name,
+            "registered_at": self.registered_at,
+            "postal_code": self.postal_code,
+            "phone": self.phone,
+            "videos_checked_out_count": self.videos_checked_out_count
+            }
---
+++ app/models/video.py
@@ -0,0 +1,16 @@
+from app import db
+# from sqlalchemy.orm import relationship
+
+class Video(db.Model):
+    video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, nullable=True)
+    total_inventory = db.Column(db.Integer, default=0)
+
+    def to_dict(self):
+        return {
+            "id": self.video_id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "total_inventory": self.total_inventory
+            }
