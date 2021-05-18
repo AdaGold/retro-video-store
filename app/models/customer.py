62fb7abd2db854b9ba6e8d5e6d01c3120c05665a
Kristin <Kristineliseboyd13@gmail.com>
2021-05-18 16:39:10 -0700

    Changed postal code from a string to an integer in the customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -5,7 +5,7 @@ from app import db
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
     phone = db.Column(db.String)
     registered_at = db.Column(db.DateTime, nullable=True)
     videos_checked_out_count = db.Column(db.Integer)
