881594d451eeb51646cdf45eae0ce902eca9f858
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-05-19 00:09:30 -0700

    Video Model updated to include available_inventory

--- app/models/video.py
+++ app/models/video.py
@@ -7,11 +7,22 @@ class Video(db.Model):
     title =  db.Column(db.String)
     release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer, nullable=True)
+
 
     def to_json(self):
-        return{
-            "id": self.video_id,
-            "title": self.title,
-            "release_date": self.release_date,
-            "total_inventory": self.total_inventory
-        }
+        if self.available_inventory:
+            return{
+                "id": self.video_id,
+                "title": self.title,
+                "release_date": self.release_date,
+                "total_inventory": self.total_inventory,
+                "available_inventory": self.available_inventory
+            }
+        else:
+            return{
+                 "id": self.video_id,
+                "title": self.title,
+                "release_date": self.release_date,
+                "total_inventory": self.total_inventory
+            }
