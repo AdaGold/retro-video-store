739d33f9fb5651171a6994abfc6dce9cfbf09af5
Lin Zhang <linzhang294@gmail.com>
2021-05-18 23:21:12 -0400

    fixes some of the models

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,8 +6,8 @@ from sqlalchemy import DateTime
 class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.Integer)
-    phone = db.Column(db.Integer)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
     registered_at = db.Column(db.DateTime)
     videos_checked_out_count = db.Column(db.Integer, default=0)
 
--- app/models/video.py
+++ app/models/video.py
@@ -6,7 +6,7 @@ from sqlalchemy import DateTime
 class Video(db.Model):
     video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    release_date = db.Column(db.DateTime)
+    release_date = db.Column(db.DateTime, nullable=True)
     total_inventory = db.Column(db.Integer)
     available_inventory = db.Column(db.Integer)
 
