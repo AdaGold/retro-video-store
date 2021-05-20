d158a778f39c164fd0aa522e2496e572612cd0be
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-20 10:45:21 -0700

    adds in models (moved to app directory)

---
+++ app/models/video.py
@@ -0,0 +1,11 @@
+from flask import current_app
+from app import db
+from sqlalchemy import DateTime
+
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, nullable=True)
+    total_inventory = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer, default=0)
