37c64d6900ac2283e09a9ee5142c6f0bd9ad6e14
x <57688724+AeronRoemer@users.noreply.github.com>
2021-05-20 15:29:30 -0600

    Customers Routes Complete for W1

--- app/routes.py
+++ app/routes.py
@@ -35,39 +35,27 @@ def post_customers():
         return make_response({"details":f"Invalid data: {err}"}, 400)
 
 @customers_bp.route("/<id>", methods=["GET"])
-def get_customer():
+def get_customer(id):
     customer = Customer.query.get_or_404(id)
     return make_response(customer.get_customer_data_structure(), 200)
 
-""" 
-@goals_bp.route("/<goal_id>", methods=["GET", "PUT", "DELETE"])
-def handle_goal(goal_id):
-    goal = Goal.query.get_or_404(goal_id)
-    if request.method == "GET":
-        return make_response(goal.goal_data_structure(), 200)
-    elif request.method == "PUT":
-        goal_data = request.get_json()
-        goal.title = goal_data["title"]
+@customers_bp.route("/<id>", methods=["PUT"])
+def update_customer(id):
+    request_body = request.get_json()
+    customer = Customer.query.get_or_404(id)
+    try:
+        customer.name=request_body["name"]
+        customer.postal_code=request_body["postal_code"]
+        customer.phone=request_body["phone"]
+        db.session.add(customer)
         db.session.commit()
-        return make_response(goal.goal_data_structure(), 200)
-    elif request.method == "DELETE":
-        db.session.delete(goal)
-        db.session.commit() 
-        goal_response = {"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}
-        return make_response(goal_response, 200)
-    
-@goals_bp.route("/<goal_id>/tasks", methods=["GET", "POST"])
-def handle_goal_tasks(goal_id):
-    goal = Goal.query.get_or_404(goal_id)
-    if request.method == "GET":
-        # Method that appends full task info
-        return make_response(goal.tasks_data_structure(), 200)
-    if request.method == "POST":
-        request_body = request.get_json()
-        for task_id in request_body["task_ids"]:
-            task = Task.query.get(task_id)
-            task.goal_id = goal_id
-            db.session.commit()
-        return make_response({"id": goal.goal_id, "task_ids": request_body["task_ids"]}, 200)
+        return make_response(customer.get_customer_data_structure(), 201)
+    except TypeError as err:
+        return make_response({"details":f"Invalid data: {err}"}, 400)
 
- """
+@customers_bp.route("/<id>", methods=["DELETE"])
+def delete_customer(id):
+    customer = Customer.query.get_or_404(id)
+    db.session.delete(customer)
+    db.session.commit() 
+    return make_response({"id": customer.id, "success": True}, 200)
