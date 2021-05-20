5bfcd1fc7df883d4088763ac3b980e698f784cfb
Maria Magana <maria.imagana15@gmail.com>
2021-05-19 21:09:34 -0700

    updated customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,7 +6,7 @@ class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     registered_at = db.Column(db.DateTime, nullable=True)
-    postal_code = db.Column(db.String)
+    postal_code = db.Column(db.Integer)
     phone = db.Column(db.String)
     videos_checked_out_count = db.Column(db.Integer, default=0)
     
