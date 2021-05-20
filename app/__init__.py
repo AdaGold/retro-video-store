4addf2baae8832a44c764008182e41ed7e39813f
Priscille Gwet <ngomakak@gmail.com>
2021-05-19 23:00:53 -0700

    Created models folder and added 3 files: customer.py,rental.py and video.py
    Added Customer model and Video model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+
+# Import models here for Alembic setup
+    from app.models.customer import Customer
+    from app.models.video import Video
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+# Register Blueprints here
+
 
     return app
---
+++ app/models/video.py
@@ -0,0 +1,20 @@
+from flask import current_app
+from app import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Video(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, nullable=True)
+    total_inventory = db.Column(db.Integer)
+    available_inventory = db.Column(db.Integer, default = 0)
+    
+    def to_json(self): 
+        return { 
+            "id": self.id,
+            "title": self.title, 
+            "release_date": self.release_date,
+            "total_inventory": self.total_inventory,
+            "available_inventory": self.available_inventory }
