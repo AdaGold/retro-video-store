483350671648261a29ce7c408a623eb1c9ed3e37
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-20 16:31:01 -0700

    add comments-question to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,9 @@ def create_app(test_config=None):
     from app.models.customer import Customer
     from app.models.video import Video
 
+    # I noticed that app works without importing Rental model in it
+    # from app.models.rental import Rental
+
     db.init_app(app)
     migrate.init_app(app, db)
 
