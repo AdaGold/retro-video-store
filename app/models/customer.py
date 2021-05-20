5685bed6a50996248f4de1f18a24ca03eec03b09
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-20 12:41:41 -0700

    fixes spelling error

--- app/models/customer.py
+++ app/models/customer.py
@@ -5,17 +5,17 @@ from sqlalchemy import DateTime
 class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.String)
     phone = db.Column(db.String)
+    postal_code = db.Column(db.String)
     registered_at = db.Column(db.DateTime)
-    video_checked_out_count = db.Column(db.Integer, default=0)
+    videos_checked_out_count = db.Column(db.Integer, default=0)
 
     def to_json(self):
         return {
             "id": self.customer_id,
             "name": self.name,
-            "registered_at": self.registered_at,
-            "postal_code": self.postal_code,
             "phone": self.phone,
-            "video_checked_out_count": self.video_checked_out_count
+            "postal_code": self.postal_code,
+            "registered_at": self.registered_at,
+            "videos_checked_out_count": self.videos_checked_out_count
         }
