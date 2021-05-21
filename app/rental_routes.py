bf3ffc55e8e6eefc6ae52b01641bd1ffe9464244
sneha agarwal <sneha8412@gmail.com>
2021-05-21 16:26:14 -0700

    added comments to check-out route

--- app/rental_routes.py
+++ app/rental_routes.py
@@ -18,14 +18,16 @@ def checkout_video_to_customers():
     
     request_body = request.get_json()
     
+    # check if the required request parameters are present in request body
     if ("customer_id" not in request_body or
         "video_id" not in request_body):
         return jsonify(details = "Invalid data"), 400
     
-    if (type(request_body["customer_id"]) is not int or 
-        type(request_body["video_id"]) is not int):
-        return jsonify(details = "Invalid data"), 400    
+    #check if the type is integer
+    if not helper.is_int(request_body["customer_id"]) or not helper.is_int(request_body["video_id"]):
+        return jsonify(details = "Invalid data"), 400      
          
+    # use the request params to get the customer and video records from the DB
     customer = Customer.query.get(request_body["customer_id"])
     if (not customer or customer == None):
         return jsonify(details = "Customer not found"), 404
@@ -37,13 +39,16 @@ def checkout_video_to_customers():
     if video.available_inventory == 0:
         return jsonify(details = " Video Not available"), 400
     
+    # update the counts for the customer and video records in the DB
     video.available_inventory -= 1 # customer checked out video
     customer.videos_checked_out_count += 1 # customer checked out a new video
-        
+    
+    #create new rental record
     new_rental = Rental(customer_id=customer.customer_id,
                             video_id=video.video_id,
                             due_date= datetime.now() + timedelta(days=7))
     
+    #add the new rental record, video and customer in the db for the customer
     db.session.add(new_rental)
     db.session.add(video)
     db.session.add(customer)
@@ -101,8 +106,6 @@ def checkin_video_to_customers():
     
     return response_body, 200
     
- 
-
 
 #OPTIONAL ENHANCEMENTS
 #GET /rentals/overdue
