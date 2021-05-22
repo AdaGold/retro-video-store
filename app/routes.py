43aa3d23a3e4d33eb407588bbc108577c04ce901
Juliana DeCarvalho Sutherland <juliana.decarvalho@wsu.edu>
2021-05-21 18:45:51 -0700

    added delete put and get methods to /<customer_id>

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ import requests
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 
+                                       ### Crud Customers ###
 @customers_bp.route("", methods=["GET"])
 def list_all_customers():
     customers = Customer.query.all()
@@ -37,4 +38,43 @@ def invalid_data(request_body):
         return True
     return False
 
+
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_customer_id(customer_id):
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response({"details": ("Customer not in the system... Want to become a member?")}, 404)
+
+    return jsonify(customer.get_customer_info())
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    request_body = request.get_json()
+
+    if customer is None:
+        return make_response({"details":("invalid data")}, 400)
+
+    customer.name = request_body["name"]
+    customer.postal_code = request_body["postal_code"]
+    customer.phone = request_body["phone"]
+
+    db.session.add(customer)
+    db.session.commit()
+
+    return make_response(customer.resp_json(), 200)
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+
+    if customer is None:
+        return make_response({"details":("Customer not in the system")}, 404)
+    
+    db.session.delete(customer)
+    db.session.commit()
+
+    return {
+        "id": f"Customer {customer.customer_id} has been erased from the universe!"
+    }, 200
 
