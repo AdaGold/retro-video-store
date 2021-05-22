86f3447c3206b8af33de87c1c0e8dc649fab8de2
Manu Ponce <manuponce@Manus-MacBook-Pro.local>
2021-05-21 19:43:00 -0700

    rental_routes.py 1/4 done

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,16 +1,14 @@
 from flask import current_app
 from app import db
 from sqlalchemy.orm import relationship
-# from datetime import datetime
+from datetime import datetime
 # from sqlalchemy import Table, Column, Integer, ForeignKey # Need?
 
 class Rental(db.Model):
     __tablename__ = "rental"
 
     rental_id =db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # video_id = db.Column(db.Integer, db.ForeignKey('customer.customer.id'), primary_key=True)
     video_id = db.Column(db.Integer, db.ForeignKey('videos.video_id'),nullable=True)
-    # customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer.id'), primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable=True)
 
     due_date = db.Column(db.DateTime, nullable=True)
--- app/rental_routes.py
+++ app/rental_routes.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 rental_bp = Blueprint("rentals", __name__, url_prefix="/rentals")
 
-
+# POST /rentals/check-out
 @rental_bp.route("/check-out", methods=["POST"])  # strict_slashes=False??
 def rental_check_out():
     request_body = request.get_json()
@@ -43,7 +43,7 @@ def rental_check_out():
     db.session.add(new_rental)
     db.session.commit()
 
-    # return new_customer.get_json(), 201
+
     return {
         "customer_id": customer.customer_id,
         "video_id": video.video_id,
@@ -51,3 +51,42 @@ def rental_check_out():
         "videos_checked_out_count": customer.videos_checked_out_count,
         "available_inventory": video.total_inventory
     },200
+
+#POST /rentals/check-in
+@rental_bp.route("/check-in", methods=["POST"])  
+def rental_check_in():
+    request_body = request.get_json()
+
+    customer = Customer.query.get(request_body.customer_id)
+    if customer == None:
+        return make_response(f"Customer {request_body.customer_id} not found", 404)
+
+    video = Video.query.get(request_body.video_id)
+    if video == None:
+        return make_response(f"Video {request_body.video_id} not found", 404)
+
+    rentals = Rental.query.filter_by(customer_id=request_body.customer_id)
+    if rentals == None:
+        return make_response(f"Rental {request_body.customer_id} not found",404)
+
+    exists=False
+    for rental in rentals:
+        if rental.video_id == request_body.video_id:
+            exists=True
+            customer.videos_checked_out_count -= 1
+            video.total_inventory += 1
+            db.session.delete(rental)
+    if exists: # if exists, 
+        db.session.commit()
+    else:
+        return make_response(f"Data {request_body.customer_id} invalid",404)
+
+#GET /customers/<id>/rentals
+@rental_bp.route("/<int:id>/rentals", methods=["GET"])  # strict_slashes=False??
+def get_customer_video_checkouts():
+
+#GET /videos/<id>/rentals
+@rental_bp.route("/<int:id>/rentals", methods=["GET"])  # strict_slashes=False??
+def get_customer_current_checkouts():
+
+
