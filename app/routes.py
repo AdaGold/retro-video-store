cae50a49b62239b88a3bcb5a12774ec3c1dffc43
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-21 19:58:33 -0700

    adds check-out routes

--- app/routes.py
+++ app/routes.py
@@ -207,34 +207,21 @@ def delete_video(video_id):
 def initiate_rental():
     rental_data = request.get_json()
 
-    # checks that customer_id for rental is an integer
-    if not is_int(rental_data["customer_id"]):
-        return make_response("Customer ID needs to be an integer", 400)
-
-    # checks that video_id for rental is an integer
-    if not is_int(rental_data["video_id"]):
-        return make_response("Video ID needs to be an integer", 400)
-
-    # checks if customer_id & "video_id" data in rental_data
-    if ("customer_id" not in rental_data.keys() or
-        "video_id" not in rental_data.keys()):
-        return make_response("Not found", 404)
+    try:
+        # converts video_id & customer_id strings into an integers (because Postman is dumb lol)
+        video_id = int(rental_data["video_id"])
+        customer_id = int(rental_data["customer_id"])
+    except ValueError or KeyError:
+        return make_response({"error": "Bad Request"}, 400)
+    
+    # checks if inventory
+    num_vids = Video.query.get(rental_data["video_id"]).index_available_inventory()
+    if num_vids == 0:
+        return make_response({"error": "Bad Request"}, 400)
     
     # generates video and customer instances
     customer = Customer.query.get(rental_data["customer_id"])
     video = Video.query.get(rental_data["video_id"])
-
-    # Conditional: Video does not exist
-    if Video is None:
-        return make_response("Video does not exist", 404)
-    
-    # Conditional: Customer does not exist
-    if Customer is None:
-        return make_response("Customer does not exist", 404)
-    
-    # Checks inventory
-    if video.index_available_inventory() == 0:
-        return make_response("No Inventory Available", 400)
     
     # Initialize rental - needs customer_id & video_id
     new_rental = Rental(customer_id = rental_data["customer_id"],
@@ -249,32 +236,46 @@ def initiate_rental():
         "customer_id": customer.customer_id,
         "video_id": video.video_id,
         "due_date": new_rental.due_date,
-        "videos_check_out_count": customer.index_checked_out(),
+        "videos_checked_out_count": customer.index_checked_out(),
         "available_inventory": video.index_available_inventory()
-    }
+    }, 200
 
-    
-    
+@rentals_bp.route("/check-in", methods=["POST"], strict_slashes=False)
+def return_rental():
+    rental_data = request.get_json()
 
+    try:
+    # converts video_id & customer_id strings into an integers (because Postman is dumb lol)
+        video_id = int(rental_data["video_id"])
+        customer_id = int(rental_data["customer_id"])
+    except ValueError or KeyError:
+        return make_response({"error": "Bad Request"}, 400)
 
-    # # check video inventory
-    # video = Video.query.get(rental_data["video_id"])
-    # if video.available_inventory == 0:
-    #     return make_response({"details": "There are no available copies of this video currently"}, 400)
-    # elif "customer_id" in rental_data and "video_id" in rental_data:
-    #     new_rental = Rental(customer_id = rental_data["customer_id"],
-    #                         video_id = rental_data["video_id"],
-    #                         due_date = rental_data["due_date"])
-    #     db.session.add(new_rental)
-        
-    #     customer = Customer.query.get(rental_data["customer_id"])
-    #     # increment customer video_checked_out_count
-    #     customer.videos_checked_out_count += 1
-    #     # decrease available video inventory
-    #     video.available_inventory -= 1
-    #     # commit changes
-    #     db.session.commit()
-        
-    #     return jsonify(new_rental.to_json), 201
+    # initializes rental
+    rental = Rental.query.filter(Rental.customer_id == rental_data["customer_id"], Rental.video_id == rental_data["video_id"]).first()
+
+    # checks if rental exists
+    if rental is None:
+        return make_response("Rental does not exist", 400)
     
-    # return make_response({"details": "Invalid data"}, 404)
+    # initializes customer
+    customer = Customer.query.get(rental_data["customer_id"])
+
+    # grabs video list from many-to-many relationship
+    video_list = customer.videos
+
+    # removes video from video_list if it matches the rental being checked-in
+    for video in video_list:
+        if video.video_id == rental_data["video_id"]:
+            video_list.remove(video)
+    
+    db.session.commit()
+
+    video = Video.query.get(rental_data["video_id"])
+
+    return {
+        "customer_id": customer.customer_id,
+        "video_id": video.video_id,
+        "videos_checked_out_count": customer.index_checked_out(),
+        "available_inventory": video.index_available_inventory()
+    }, 200
