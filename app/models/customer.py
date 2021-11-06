2c0119c5a7e3be37d1a53761a2a33b7360704b28
Roslyn Melookaran <rmelookaran@gmail.com>
2021-11-05 18:29:39 -0600

    added /customers route

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,3 +6,14 @@ class Customer(db.Model):
     name = db.Column(db.String)
     postal_code = db.Column(db.Integer)
     phone = db.Column(db.String)
+    register_at = db.Column(db.DateTime)
+
+
+    def to_dict(self):
+        # is_complete = False if not self.completed_at else True
+        return {
+                "name": self.name,
+                "id": self.id,
+                "phone": self.phone,
+                "postal_code": str(self.postal_code)
+                }
--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,31 @@ rental_bp = Blueprint("rental_bp", __name__, url_prefix="/rentals")
 
 
 #Roslyn: Customer
+@customer_bp.route("", methods=["GET", "POST"])
+def handle_customers():
+    customer_response = []
+    if request.method == "GET":
+        if request.args.get("sort") == "asc":
+            customers = Customer.query.order_by(Customer.title.asc())
+        elif request.args.get("sort") == "desc":
+            customers = Customer.query.order_by(Customer.title.desc())
+        else:
+            customers = Customer.query.all()
+        customer_response = [customer.to_dict() for customer in customers]
+        return jsonify(customer_response), 200
+    elif request.method == "POST":
+        request_body = request.get_json()
+        if "name" not in request_body or "phone" not in request_body or "postal_code" not in request_body:
+            return make_response({"details": "Invalid data"}, 400) 
+        new_customer = Customer(name=request_body["name"],
+                        phone=request_body["phone"], 
+                        postal_code=request_body["postal_code"],
+                        register_at=datetime.utcnow())
+        db.session.add(new_customer)
+        db.session.commit()
+        return make_response({"customer": new_customer.to_dict()}, 201)
+
+
 #Areeba: Video 
 
 # extra note
