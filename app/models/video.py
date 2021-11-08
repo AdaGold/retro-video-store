96b920cb6dc365edf5a3297907eb889a12a86bf4
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-11-08 07:35:10 -0800

    rough CRUD for video needs DRYing all tests pass

--- app/models/video.py
+++ app/models/video.py
@@ -1,5 +1,6 @@
 # from flask import current_app
 from app import db
+from sqlalchemy.schema import FetchedValue
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,3 +8,11 @@ class Video(db.Model):
     release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
     customers = db.relationship("Customer", secondary="videos_customers", backref="videos")
+
+    def to_dict(self):
+        return {
+                "title": self.title,
+                "id": self.id,
+                "release_date": self.release_date,
+                "total_inventory": self.total_inventory
+                }
--- app/routes.py
+++ app/routes.py
@@ -17,11 +17,11 @@ rental_bp = Blueprint("rental_bp", __name__, url_prefix="/rentals")
 customer_keys = ["name", "phone", "postal_code"]
 
 #Roslyn: Customer
-@customer_bp.route("", methods=["GET", "POST"])
+@customer_bp.route("", methods=["GET", "POST"]) #Roslyn - I think we should break the functions up for each method
 def handle_customers():
     customer_response = []
     if request.method == "GET":
-        if request.args.get("sort") == "asc": # We can delete "asc" and "desc" sorting if you want, I just carried it over from the task proj
+        if request.args.get("sort") == "asc": #Roslyn - I think you may be able to use my helper function for this
             customers = Customer.query.order_by(Customer.title.asc())
         elif request.args.get("sort") == "desc":
             customers = Customer.query.order_by(Customer.title.desc())
@@ -81,5 +81,107 @@ def check_data(check_items, request_body): # Areeba - I think you could use this
 def not_found_response(entity, id): # Areeba - you could use this and fill in "Video" as the "entity" parameter, or if you think this method is confusing or clunky, we can forego this function
     return make_response({"message" : f"{entity} {id} was not found"}, 404)
 
+#----------------------------------------------------------------------------------#
 #Areeba: Video 
+
+video_keys = ["title", "release_date", "total_inventory"]
+
+@video_bp.route("", methods=["GET"])
+def read_videos():
+    videos_response = []
+    #this part can go in a helper function, we can also create sort by release date
+    # if request.args.get("sort") == "asc": 
+    #     videos = Video.query.order_by(Video.title.asc())
+    # elif request.args.get("sort") == "desc":
+    #     videos = Video.query.order_by(Video.title.desc())
+    # else:
+    #     videos = Video.query.all()
+    videos = sort_titles(request.args.get("sort"), Video)
+    #print(videos)
+    videos_response = [video.to_dict() for video in videos]
+    return jsonify(videos_response), 200
+
+@video_bp.route("", methods=["POST"])
+def create_video():
+    request_body = request.get_json()
+    is_complete = check_data(video_keys, request_body)
+    if is_complete:
+        return is_complete
+    else:
+        new_video = Video(title=request_body["title"],
+                            release_date=request_body["release_date"], 
+                            total_inventory=request_body["total_inventory"])
+        db.session.add(new_video)
+        db.session.commit()
+        return make_response(new_video.to_dict(), 201)
+
+def sort_titles(sort_by, entity):
+    #Thinking about making this a very generic function to sort anything with a simple order_by 
+    if sort_by == "asc": 
+        sorted = entity.query.order_by(entity.title.asc())
+    elif sort_by == "desc":
+        sorted = entity.query.order_by(entity.title.desc())
+    else:
+        sorted = entity.query.all()
+    return sorted
+
+def sort_dates(sort_by):
+    #May want to use this for release_dates if sort_titles can'st be made generic?
+    pass 
+
+
+@video_bp.route("/<video_id>", methods=["GET"])
+def read_a_video(video_id):
+    if not video_id.isnumeric():
+        return make_response({"message" : "Please enter a valid video id"}, 400)
+    
+    video = Video.query.get(video_id)
+    return not_found_response("Video", video_id) if not video else make_response(video.to_dict(),200)
+
+def id_check(id):
+    pass
+    #DRYing code
+    # if not id.isnumeric():
+    #     return make_response({"message" : "Please enter a valid customer id"}, 400)
+    # else:
+    #     return True
+
+
+@video_bp.route("/<video_id>", methods=["PUT"])
+def update_video(video_id):
+    if not video_id.isnumeric():
+        return make_response({"message" : "Please enter a valid video id"}, 400)
+    
+    video = Video.query.get(video_id) 
+    
+    if not video:
+        return not_found_response("Video", video_id)
+
+    request_body = request.get_json()
+
+    is_complete = check_data(video_keys, request_body)
+
+    if is_complete:
+        return is_complete
+    video.title = request_body["title"]
+    video.release_date = request_body["release_date"]
+    video.total_inventory = request_body["total_inventory"]
+    db.session.commit()
+    return make_response(video.to_dict(), 200)
+    
+
+@video_bp.route("/<video_id>", methods=["DELETE"])
+def delete_video(video_id):
+    if not video_id.isnumeric():
+        return make_response({"message" : "Please enter a valid video id"}, 400)
+
+    video = Video.query.get(video_id) 
+
+    if not video:
+        return not_found_response("Video", video_id)
+    db.session.delete(video)
+    db.session.commit()
+    #return make_response({"id": int(video_id)}, 200)
+    response = {"id": video.id}
+    return make_response(response, 200)
 
