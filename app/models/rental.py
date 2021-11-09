9cb8fc8457c230d532f6d709cd00606a5ac66dac
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-11-09 10:42:57 -0800

    all but 1 test passing :)

--- app/models/rental.py
+++ app/models/rental.py
@@ -3,9 +3,9 @@ from app import db
 
 class Rental(db.Model):
     __tablename__ = "videos_customers"
-    #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     due_date = db.Column(db.DateTime)
 
     def to_dict(self, checked_out, available_inventory):
--- app/routes.py
+++ app/routes.py
@@ -208,19 +208,23 @@ def handle_check_out():
     
     else: 
         video_id = request_body["video_id"]
-        customer_id = request_body["customer_id"]
         video = Video.query.get(video_id)
         #customer = Customer.query.get(customer_id)
-        if video.total_inventory == 0 or video.total_inventory is None:
-            return make_response({"message": "Could not perform checkout"}, 400)
-        
+        # if video.total_inventory == 0 or video.total_inventory is None:
+        #     return result
+        #if video is not None and video.total_inventory > 0:
+        customer_id = request_body["customer_id"]
         video.inventory_checked_out = video.inventory_checked_out + 1
         inventory_available = video.total_inventory - video.inventory_checked_out
         today = datetime.now(timezone.utc)
         RENTAL_PERIOD = 7
         due_date = today + timedelta(days=RENTAL_PERIOD)
         
-        new_rental = Rental(video_id=video_id, customer_id=customer_id, due_date=due_date)
+        new_rental = Rental(
+        video_id=video_id, 
+        customer_id=customer_id, 
+        due_date=due_date)
+
         db.session.add(new_rental)
         db.session.commit()
         return make_response(new_rental.to_dict(checked_out=video.inventory_checked_out, available_inventory=inventory_available), 200)
@@ -270,37 +274,6 @@ def create_customer(request_body):
         db.session.commit()
         return make_response(new_customer.to_dict(), 201)
 
-# def create_check_out(request_body):
-#     video_id = request_body["video_id"]
-#     customer_id = request_body["customer_id"]
-#     video = Video.query.get(video_id)
-#     customer = Customer.query.get(customer_id)
-#     if video.total_inventory > 0:
-#         video.inventory_checked_out += 1
-#         inventory_available = video.total_inventory - video.inventory_checked_out
-#     else: 
-#         return make_response({"message": "Could not perform checkout"}, 400)
-    
-#     today = datetime.now(timezone.utc)
-#     RENTAL_PERIOD = 7
-#     due_date = today + timedelta(days=RENTAL_PERIOD)
-    
-#     new_rental = Rental(video_id=video_id, customer_id=customer_id, due_date=due_date)
-#     db.session.add(new_rental)
-#     db.session.commit()
-#     return make_response(new_rental.to_dict(checked_out=video.inventory_checked_out, available_inventory=inventory_available), 200)
-
-# def create_check_in(request_body):
-#     video_id = request_body["video_id"]
-#     customer_id = request_body["customer_id"]
-#     video = Video.query.get(video_id)
-#     video.inventory_checked_out-=1
-#     inventory_available = video.total_inventory - video.inventory_checked_out
-#     rental = Rental.query.get()
-#     db.session.delete(rental)
-#     db.session.commit()
-#     return make_response(rental.to_dict(checked_out=video.inventory_checked_out, available_inventory=inventory_available), 200)
-
 def check_data(check_items, request_body): 
     for key in check_items:
         if key not in request_body.keys():
@@ -336,200 +309,40 @@ def check_rental_data(request_body):
     video = Video.query.get(request_body["video_id"])
     customer = Customer.query.get(request_body["customer_id"])
     if not video or not customer:
-            return make_response({"details": "The video or customer id you have entered is incorrect"}, 404)
+        return make_response({"details": "The video or customer id you have entered is incorrect"}, 404)
+    # if video.total_inventory == 0 or video.total_inventory is None:
+    #     return make_response({"message": "Could not perform checkout"}, 400) 
     return False
 
 #DRY for error checks: https://stackoverflow.com/questions/38488476/a-dry-approach-to-python-try-except-blocks
 
-
-
-####
-####
-####
-####
-####
-
-
-#import datetime
-# from typing import ChainMap
-# from app import db
-# from app.models.video import Video
-# from app.models.customer import Customer
-# from app.models.rental import Rental
-# from flask import request, Blueprint, make_response, jsonify
-# from datetime import datetime
-# from dotenv import load_dotenv
-# import os
-# import requests
-
-# video_bp = Blueprint("video_bp", __name__, url_prefix="/videos")
-# customer_bp = Blueprint("customer_bp", __name__, url_prefix="/customers")
-# rental_bp = Blueprint("rental_bp", __name__, url_prefix="/rentals")
-
-# customer_keys = ["name", "phone", "postal_code"]
-
-# #Roslyn: Customer
-# @customer_bp.route("", methods=["GET"]) #Roslyn - I think we should break the functions up for each method
-# def read_customers():
-#     customer_response = []
-#     customers = sort_titles(request.args.get("sort"), Customer)
-#     customer_response = [customer.to_dict() for customer in customers]
-#     return jsonify(customer_response), 200
-
-# @customer_bp.route("", methods=["POST"]) #Roslyn - I think we should break the functions up for each method
-# def create_customer():
-#     request_body = request.get_json()
-#     is_complete = check_data(customer_keys, request_body)
-#     return is_complete if is_complete else create_customer(request_body)
-
-# @customer_bp.route("/<customer_id>", methods=["GET"])
-# def read_a_customer(customer_id):
-#     response = id_check(customer_id)
-#     if response:
-#         return response
-#     customer = Customer.query.get(customer_id)
-#     return not_found_response("Customer", customer_id) if not customer else make_response(customer.to_dict(),200)
-    
-# @customer_bp.route("/<customer_id>", methods=["DELETE"])
-# def delete_a_customer(customer_id):
-#     response = id_check(customer_id)
-#     if response:
-#         return response
-#     customer = Customer.query.get(customer_id)
-#     if not customer:
-#         return not_found_response("Customer", customer_id)
-#     db.session.delete(customer)
-#     db.session.commit()
-#     return make_response({"id": int(customer_id)}, 200)
-
-# @customer_bp.route("/<customer_id>", methods=["PUT"])
-# def update_a_customer(customer_id):
-#     response = id_check(customer_id)
-#     if response:
-#         return response
-#     customer = Customer.query.get(customer_id)
-#     if not customer:
-#         return not_found_response("Customer", customer_id)
-#     request_body = request.get_json()
-#     is_complete = check_data(customer_keys, request_body)
-#     if is_complete:
-#         return is_complete
-#     customer.name = request_body["name"]
-#     customer.phone = request_body["phone"]
-#     customer.postal_code = request_body["postal_code"]
-#     db.session.commit()
-#     return make_response(customer.to_dict(), 200)
-
-# def create_customer(request_body):
-#         new_customer = Customer(name=request_body["name"],
-#                             phone=request_body["phone"], 
-#                             postal_code=request_body["postal_code"],
-#                             register_at=datetime.utcnow())
-#         db.session.add(new_customer)
-#         db.session.commit()
-#         return make_response(new_customer.to_dict(), 201)
-
-
-# def check_data(check_items, request_body): # Areeba - I think you could use this to check video "PUT" and "POST" request data too
-#     for key in check_items:
-#         if key not in request_body.keys():
-#             return make_response({"details": f"Request body must include {key}."}, 400)
-#     return False
-
-
-# def not_found_response(entity, id): # Areeba - you could use this and fill in "Video" as the "entity" parameter, or if you think this method is confusing or clunky, we can forego this function
-#     return make_response({"message" : f"{entity} {id} was not found"}, 404)
-
-# #----------------------------------------------------------------------------------#
-# #Areeba: Video 
-
-# video_keys = ["title", "release_date", "total_inventory"]
-
-# @video_bp.route("", methods=["GET"])
-# def read_videos():
-#     videos_response = []
-#     videos = sort_titles(request.args.get("sort"), Video)
-#     videos_response = [video.to_dict() for video in videos]
-#     return jsonify(videos_response), 200
-
-
-# @video_bp.route("", methods=["POST"])
-# def create_video():
-#     request_body = request.get_json()
-#     is_complete = check_data(video_keys, request_body)
-#     if is_complete:
-#         return is_complete
-#     else:
-#         new_video = Video(title=request_body["title"],
-#                             release_date=request_body["release_date"], 
-#                             total_inventory=request_body["total_inventory"])
-#         db.session.add(new_video)
-#         db.session.commit()
-#         return make_response(new_video.to_dict(), 201)
-
-# #These could be instance methods?
-# def sort_titles(sort_by, entity):
-#     #Thinking about making this a very generic function to sort anything with a simple order_by 
-#     if sort_by == "asc": 
-#         sorted = entity.query.order_by(entity.title.asc())
-#     elif sort_by == "desc":
-#         sorted = entity.query.order_by(entity.title.desc())
-#     else:
-#         sorted = entity.query.all()
-#     return sorted
-
-# def sort_dates(sort_by):
-#     #May want to use this for release_dates if sort_titles can't be made generic?
-#     pass 
-
-
-# @video_bp.route("/<video_id>", methods=["GET"])
-# def read_a_video(video_id):
-#     if not video_id.isnumeric():
-#         return make_response({"message" : "Please enter a valid video id"}, 400)
+# def create_check_out(request_body):
+#     video_id = request_body["video_id"]
+#     customer_id = request_body["customer_id"]
 #     video = Video.query.get(video_id)
-#     return not_found_response("Video", video_id) if not video else make_response(video.to_dict(),200)
-
-# def id_check(id):
-#     # DRYing code
-#     response = make_response({"message" : "Please enter a valid customer id"}, 400)
-#     return response if not id.isnumeric() else False
-
-
-# @video_bp.route("/<video_id>", methods=["PUT"])
-# def update_video(video_id):
-#     if not video_id.isnumeric():
-#         return make_response({"message" : "Please enter a valid video id"}, 400)
+#     customer = Customer.query.get(customer_id)
+#     if video.total_inventory > 0:
+#         video.inventory_checked_out += 1
+#         inventory_available = video.total_inventory - video.inventory_checked_out
+#     else: 
+#         return make_response({"message": "Could not perform checkout"}, 400)
     
-#     video = Video.query.get(video_id) 
+#     today = datetime.now(timezone.utc)
+#     RENTAL_PERIOD = 7
+#     due_date = today + timedelta(days=RENTAL_PERIOD)
     
-#     if not video:
-#         return not_found_response("Video", video_id)
-
-#     request_body = request.get_json()
-
-#     is_complete = check_data(video_keys, request_body)
-
-#     if is_complete:
-#         return is_complete
-#     video.title = request_body["title"]
-#     video.release_date = request_body["release_date"]
-#     video.total_inventory = request_body["total_inventory"]
+#     new_rental = Rental(video_id=video_id, customer_id=customer_id, due_date=due_date)
+#     db.session.add(new_rental)
 #     db.session.commit()
-#     return make_response(video.to_dict(), 200)
-    
+#     return make_response(new_rental.to_dict(checked_out=video.inventory_checked_out, available_inventory=inventory_available), 200)
 
-# @video_bp.route("/<video_id>", methods=["DELETE"])
-# def delete_video(video_id):
-#     if not video_id.isnumeric():
-#         return make_response({"message" : "Please enter a valid video id"}, 400)
-
-#     video = Video.query.get(video_id) 
-
-#     if not video:
-#         return not_found_response("Video", video_id)
-#     db.session.delete(video)
+# def create_check_in(request_body):
+#     video_id = request_body["video_id"]
+#     customer_id = request_body["customer_id"]
+#     video = Video.query.get(video_id)
+#     video.inventory_checked_out-=1
+#     inventory_available = video.total_inventory - video.inventory_checked_out
+#     rental = Rental.query.get()
+#     db.session.delete(rental)
 #     db.session.commit()
-#     #return make_response({"id": int(video_id)}, 200)
-#     response = {"id": video.id}
-#     return make_response(response, 200)
+#     return make_response(rental.to_dict(checked_out=video.inventory_checked_out, available_inventory=inventory_available), 200)
