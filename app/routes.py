3419e43de9b45228aa4fff4f6bee3278caf35450
Sandra Caballero <spcaballero.2@gmail.com>
2021-11-10 16:52:14 -0800

    passing wave 1

--- app/routes.py
+++ app/routes.py
@@ -2,50 +2,107 @@ from app import db
 from flask import Blueprint, jsonify, request
 from datetime import datetime
 from app.models.video import Video
-
-video_bp = Blueprint("videos", __name__, url_prefix=("/videos"))
-#within the empty quotes for the route do I need to put "/<video>"
-@video_bp.route("", methods=["POST"])
-def post_video():
-    if request.method == "POST":
-        request_body = request.get_json()
-        if "title" not in request_body or "release_date" not in request_body or "total_inventory" not in request_body:
-                    return{
-                        "details": "invalid data"
-                    }, 400
-
-db.session.add()
-db.session.commit()
-
-@video_bp.route("", methods=["GET"])
-def get_video():
-#list = []
- for video in list:
-            ({
-            "id": video.video_id,
-            "title": video.title,
-            "release_date": datetime.now(),
-            "total_inventory": video.total_inventory
-            })
-
-@video_bp.route("/video/<id>",methods=["GET"])
-# Gives back details about specific video 
-# in the store's inventory.
-#response 
-            ({
-            "id": video.video_id,
-            "title": video.title,
-            "release_date": datetime.now(),
-            "total_inventory": video.total_inventory
-            })
-
-@video_bp.route("/video<id>",methods=["PUT"])
-#this will update/replace the video id 
-#should it return the same as 35-40 as above
-
-from app.models import customer
 from app.models.customer import Customer
-import requests
+
+
+videos_bp = Blueprint("videos", __name__, url_prefix=("/videos"))
+
+#POST/CREATE 
+@videos_bp.route("",methods=["POST"])
+def post_videos():
+    request_body = request.get_json()
+    if "title" not in request_body:
+        return{
+            "details": "Request body must include title."
+        }, 400
+    
+    elif "release_date" not in request_body:
+        return{
+            "details": "Request body must include release_date."
+        }, 400
+    
+    elif "total_inventory" not in request_body:
+        return{
+            "details": "Request body must include total_inventory."
+        }, 400
+
+    new_video = Video(
+        title = request_body["title"], 
+        release_date = request_body["release_date"],
+        total_inventory = request_body["total_inventory"]
+    )
+    db.session.add(new_video)
+    db.session.commit()
+
+    return jsonify(new_video.to_video_object()), 201
+
+#GET
+@videos_bp.route("", methods=["GET"])
+def get_videos():
+    videos = Video.query.all()
+    videos_response = []
+    for video in videos:
+        videos_response.append(video.to_video_object())
+    return jsonify(videos_response), 200
+
+@videos_bp.route("/<video_id>",methods=["GET"])
+def get_video(video_id):
+    if video_id.isnumeric() != True:
+        return {"message": "Video id provided is not a number."}, 400
+    
+    video = Video.query.get(video_id)
+
+    if video is None:
+        return {"message": f"Video {video_id} was not found"}, 404
+        
+
+    return{
+        "id": video.id,
+        "title": video.title,
+        "release_date": datetime.now(),
+        "total_inventory": video.total_inventory
+    }
+#PUT
+@videos_bp.route("/<video_id>",methods=["PUT"])
+def update_video(video_id):
+    video = Video.query.get(video_id)
+    request_body = request.get_json()
+
+    if video is None:
+        return {"message": f"Video {video_id} was not found"}, 404
+
+    if "title" not in request_body or "release_date" not in request_body or "total_inventory" not in request_body:
+        return {
+            "details": "Invalid data"
+        }, 400
+
+    video.title = request_body["title"]
+    video.release_date = request_body["release_date"]
+    video.total_inventory = request_body["total_inventory"]
+
+    db.session.commit()
+
+    return {
+        "id": video.id,
+        "title": video.title,
+        "release_date": datetime.now(),
+        "total_inventory": video.total_inventory
+    }
+#DELETE
+@videos_bp.route("/<video_id>", methods=["DELETE"])
+def delete_video(video_id):
+
+    video = Video.query.get(video_id)
+
+    if video is None:
+        return {"message": f"Video {video_id} was not found"}, 404
+
+    db.session.delete(video)
+    db.session.commit()
+
+    return jsonify({"id": video.id}), 200
+
+###Customer
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 
@@ -112,7 +169,7 @@ def update_customer(customer_id):
     request_body = request.get_json()
     
     if customer is None:
-        return {"message": f"Customer {customer.id} was not found"}, 404
+        return {"message": f"Customer {customer_id} was not found"}, 404
 
     if "name" not in request_body or "postal_code" not in request_body or "phone" not in request_body:
         return {
